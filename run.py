e96834dfd93f35537e33d04cacf44e566de6d062
Scott Maxwell <scott@scotteh.me>
2022-04-06 20:26:47 +0100

    Bump version number to 0.0.6

--- run.py
+++ run.py
@@ -1,17 +1,6 @@
 from pyshowdown import client
 import asyncio
-import ssl
-import certifi
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_verify_locations(certifi.where())
-
-
-async def main():
-    c = client.Client("sim3.psim.us", 443, "/showdown/websocket", ssl_context=context)
-    await c.keep_connected()
-
-
 if __name__ == "__main__":
+    c = client.Client("sim3.psim.us", 443, "/showdown/websocket")
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(c.keep_connected())
